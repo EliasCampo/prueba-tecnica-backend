043808ede8cac7f97f1917fa9ac2a599ecf64308
EliasCampo <eliascampo014@gmail.com>
2025-09-23 14:53:36 -0300

    add endpoint GET

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
+app.use(express.json());
+
+app.use(cors());
+app.listen(8000);
 
 
-app.get('/', (req, res)=>{
-    res.send('Hello World');
+let task = [
+    {
+        id: 1, 
+        title: 'tarea 1',
+        description: 'descripcion de la tarea 1',
+        priority: 'hard'
+    },
+    {
+        id: 2, 
+        title: 'tarea 2',
+        description: 'descripcion de la tarea 2',
+        priority: 'medium'
+    }, 
+];
+
+app.get('/tasks', (req, res)=>{
+    res.json(task);
 })
 
 
@@ -12,4 +32,3 @@ app.get('/', (req, res)=>{
 
 
 
-app.listen(8000);
